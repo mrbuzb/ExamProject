5a6305715dcfc449a357a44b15adf2502bdcc90e
mrbuzb <mrbpubg14@gmail.com>
2025-06-13 10:04:12 +0500

    EntitiesCreated

---
+++ src/ToDoList.Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿namespace ToDoList.Domain.Entities;
+
+public class User
+{
+    public long UserId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Salt { get; set; }
+
+    public long RoleId { get; set; }
+    public UserRole Role { get; set; }
+
+    public ICollection<ToDoItem> ToDoItems { get; set; }
+    public ICollection<RefreshToken> RefreshTokens { get; set; }
+}
---
+++ src/ToDoList.Domain/Entities/UserRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Domain.Entities;
+
+public class UserRole
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public ICollection<User> Users { get; set; }
+}
