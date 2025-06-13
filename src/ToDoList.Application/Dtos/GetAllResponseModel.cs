9866215edab8886e9c8dc7ec8e9a07d12bd93a38
mrbuzb <mrbpubg14@gmail.com>
2025-06-13 10:19:34 +0500

    Dtos Created

---
+++ src/ToDoList.Application/Dtos/GetAllResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ToDoList.Application.Dtos;
+
+public class GetAllResponseModel
+{
+    public int TotalCount { get; set; }
+    public List<ToDoItemGetDto> ToDoItemGetDtos { get; set; }
+}
