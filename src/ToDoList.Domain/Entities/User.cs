6daf15cb75af83222417767ce042f44f9e5f0ffa
mrbuzb <mrbpubg14@gmail.com>
2025-06-13 10:13:08 +0500

    Configurations Writed

--- src/ToDoList.Domain/Entities/User.cs
+++ src/ToDoList.Domain/Entities/User.cs
@@ -9,7 +9,7 @@ public class User
     public string Password { get; set; }
     public string PhoneNumber { get; set; }
     public string Salt { get; set; }
-
+    public string Email { get; set; }
     public long RoleId { get; set; }
     public UserRole Role { get; set; }
 
