33037448c06411b14764af5ab85ba0c5b9765c11
mrbuzb <mrbpubg14@gmail.com>
2025-06-13 11:12:41 +0500

    Interfaces writed and implemented

---
+++ src/ToDoList.Application/Interfaces/IRoleRepository.cs
@@ -0,0 +1,10 @@
+﻿using ToDoList.Domain.Entities;
+
+namespace ToDoList.Application.Interfaces;
+
+public interface IRoleRepository
+{
+    Task<ICollection<User>> GetAllUsersByRoleAsync(string role);
+    Task<List<UserRole>> GetAllRolesAsync();
+    Task<long> GetRoleIdAsync(string role);
+}
---
+++ src/ToDoList.Infrastructure/Persistence/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Application.Interfaces;
+using ToDoList.Domain.Entities;
+
+namespace ToDoList.Infrastructure.Persistence.Repositories;
+
+public class RefreshTokenRepository : IRefreshTokenRepository
+{
+    public Task AddRefreshToken(RefreshToken refreshToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteRefreshToken(string refreshToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<RefreshToken> SelectRefreshToken(string refreshToken, long userId)
+    {
+        throw new NotImplementedException();
+    }
+}
