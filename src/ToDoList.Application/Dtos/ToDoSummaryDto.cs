3f83664774d5df9ee6f0f34873be5380082161c9
Obidjon4554 <obidjoninha@gmail.com>
2025-06-18 16:18:23 +0500

    add summary endpoint to return total, completed, incompleted, and overdue ToDo counts

---
+++ src/ToDoList.Application/Dtos/ToDoSummaryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoList.Application.DTOs;
+
+public class ToDoSummaryDto
+{
+    public int Total { get; set; }
+    public int Completed { get; set; }
+    public int Incompleted { get; set; }
+    public int Overdue { get; set; }
+}
