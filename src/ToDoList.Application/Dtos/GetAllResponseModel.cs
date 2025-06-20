222bf9696ab6684e5bf1e799c3e25d61df67befd
mrbuzb <mrbpubg14@gmail.com>
2025-06-20 18:52:21 +0500

    RemoveModel

--- src/ToDoList.Application/Dtos/GetAllResponseModel.cs
+++
@@ -1,8 +0,0 @@
-﻿ namespace ToDoList.Application.Dtos;
-
-public class GetAllResponseModel
-{
-    public int TotalCount { get; set; }
-
-    public List<ToDoItemGetDto> ToDoItemGetDtos { get; set; }
-}
