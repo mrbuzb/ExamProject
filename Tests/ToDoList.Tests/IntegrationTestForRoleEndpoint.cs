cb3273bcdc715322e94a0f74e809cd8329854f0f
Nurulloh99 <nurullohlutfullayev1999@gmail.com>
2025-06-21 08:26:51 +0500

    IntegrationTestForRoleEndpoint has been completed

---
+++ Tests/ToDoList.Tests/IntegrationTestForRoleEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using ToDoList.Api;
+
+namespace ToDoList.Tests;
+
+public class IntegrationTestForRoleEndpoint : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public IntegrationTestForRoleEndpoint(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task GetAllRoles_ReturnsUnauthorized_WhenUserIsNotAuthenticated()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/api/role/get-all-roles");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+}
